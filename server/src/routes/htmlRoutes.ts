9996a4e9a69bd9f3d2d58b9b62f62fa60b16d66b
kurlee710 <josephatamaniuk@gmail.com>
2024-09-27 15:38:49 -0400

    fixed route to serve index.html

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -6,10 +6,8 @@ const __dirname = path.dirname(__filename);
 const router = Router();
 
 // TODO: Define route to serve index.html
-router.get("/", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "path_to_your_html_directory", "index.html")
-  );
+router.get("/", (_req, res) => {
+  res.sendFile(path.join(__dirname, "../../../client/dist/index.html"));
 });
 
 export default router;
